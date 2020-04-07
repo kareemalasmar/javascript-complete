109a7b15ebeaab00f6dca6c8f42981bfa189903a
KA <kareemalasmar@gmail.com>
2020-04-06 19:29:12 -0700

    dependency fix

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,15 @@
-console.log('hello friend...');
+// ********************************************
+// Lecture # 7
+// ********************************************
+
+var firstName = 'John';
+
+console.log(firstName);
+
+var lastName = 'Smith';
+
+var age = 28;
+
+console.log(firstName + ' ' + lastName + ' is ' + age + ' years old.');
+
+// ############################################
