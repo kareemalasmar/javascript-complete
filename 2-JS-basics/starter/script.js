258ec4d92d363170847b96e1f9f2e13bcddd3ba9
KA <kareemalasmar@gmail.com>
2020-04-06 22:10:27 -0700

    variables data types and operators

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,15 +1,55 @@
 // ********************************************
-// Lecture # 7
+// Lecture # 7 & 8    Variable and Data Types
 // ********************************************
 
-var firstName = 'John';
+// var firstName = 'John';
 
-console.log(firstName);
+// console.log(firstName);
 
-var lastName = 'Smith';
+// var lastName = 'Smith';
 
-var age = 28;
+// var age = 28;
+// console.log(firstName + ' ' + lastName + ' is ' + age + ' years old.');
 
-console.log(firstName + ' ' + lastName + ' is ' + age + ' years old.');
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+
+// console.log(job);
+
+// ############################################
+// Lecture # 9      Basic Operators
+// ********************************************
+
+var year, yearJohn, yearMark, ageJohn, ageMark;
+now = 2020;
+ageJohn = 28;
+ageMark = 33;
+
+// Math Operators
+yearJohn = now - ageJohn;
+yearMark = now - ageMark;
+
+console.log(yearJohn);
+console.log(now + 2);
+console.log(now - 2);
+console.log(now * 2);
+console.log(now / 2);
+
+// Logical Operator
+
+var johnOlder = ageJohn > ageMark;
+console.log(johnOlder);
+
+// Type of Operator
+var friend = 'Sam';
+var address;
+
+console.log(typeof address);
+
+// ############################################
+// Lecture #
+// ********************************************
 
 // ############################################
