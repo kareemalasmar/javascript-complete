e5ec90c05b6b92d92ad840280adeb7fdd9658ab3
KA <kareemalasmar@gmail.com>
2020-04-06 23:31:22 -0700

    challenge 1

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -22,31 +22,100 @@
 // Lecture # 9      Basic Operators
 // ********************************************
 
-var year, yearJohn, yearMark, ageJohn, ageMark;
-now = 2020;
-ageJohn = 28;
-ageMark = 33;
+// var year, yearJohn, yearMark, ageJohn, ageMark;
+// now = 2020;
+// ageJohn = 28;
+// ageMark = 33;
 
-// Math Operators
-yearJohn = now - ageJohn;
-yearMark = now - ageMark;
+// // Math Operators
+// yearJohn = now - ageJohn;
+// yearMark = now - ageMark;
 
-console.log(yearJohn);
-console.log(now + 2);
-console.log(now - 2);
-console.log(now * 2);
-console.log(now / 2);
+// console.log(yearJohn);
+// console.log(now + 2);
+// console.log(now - 2);
+// console.log(now * 2);
+// console.log(now / 2);
 
-// Logical Operator
+// // Logical Operator
 
-var johnOlder = ageJohn > ageMark;
-console.log(johnOlder);
+// var johnOlder = ageJohn > ageMark;
+// console.log(johnOlder);
 
-// Type of Operator
-var friend = 'Sam';
-var address;
+// // Type of Operator
+// var friend = 'Sam';
+// var address;
 
-console.log(typeof address);
+// console.log(typeof address);
+
+// ############################################
+// Lecture # 11 Operator Precedence and Associativity
+// ********************************************
+
+// var now = 2018;
+// var yearJohn = 1989;
+// var fullAge = 18;
+
+// var isFullAge = now - yearJohn >= fullAge;
+
+// console.log(isFullAge);
+
+// var ageJohn = now - yearJohn;
+// var ageMark = 35;
+
+// var average = (ageJohn + ageMark) / 2;
+// console.log(average);
+
+// var x, y;
+// x = y = (3 + 5) * 4 - 6; // 8 * 4 -6 // 32 - 6// 26
+// console.log(x, y);
+
+// // More operators
+// x = x * 2
+// X *= 2 // both of these show the same and works with other basic operators
+
+// y++; // y + 1
+
+// ############################################
+// Lecture # 12 Coding challenge
+// ********************************************
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
+
+GOOD LUCK 😀
+*/
+
+// var massMark = 78; // kg
+// var heightMark = 1.69; // meters
+
+// var massJohn = 92;
+// var heightJohn = 1.95;
+
+// var BMIMark, BMIJohn;
+
+// BMIMark = massMark / heightMark ** 2;
+// BMIJohn = massJohn / heightJohn ** 2;
+
+// var BMIMarkHigher = BMIMark > BMIJohn;
+
+// console.log(
+//   "Mark's BMI is " +
+//     BMIMark +
+//     " and John's BMI is " +
+//     BMIJohn +
+//     ". Is Mark's higher than John's? " +
+//     BMIMarkHigher
+// );
+
+// ############################################
+// Lecture #
+// ********************************************
 
 // ############################################
 // Lecture #
