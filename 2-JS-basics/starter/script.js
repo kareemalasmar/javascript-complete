06b2cff886ea081ed05864ebb87a4d0c432a351f
KA <kareemalasmar@gmail.com>
2020-04-07 21:23:59 -0700

    coding challenge 2

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -202,6 +202,49 @@ GOOD LUCK 😀
 // === no type coercion
 
 // ############################################
+// Lecture # 18 Coding Challenge 2
+// ********************************************
+
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+
+GOOD LUCK 😀
+*/
+
+// var averageJohn, averageMike, averageMary;
+
+// averageJohn = (60 + 120 + 103) / 3;
+// averageMike = (89 + 120 + 103) / 3;
+// averageMary = (89 + 120 + 103) / 3;
+
+// console.log(
+//   'John: ' +
+//     averageJohn +
+//     '. Mike: ' +
+//     averageMike +
+//     '. Mary: ' +
+//     averageMary +
+//     '.'
+// );
+
+// if (averageJohn > averageMike && averageJohn > averageMary) {
+//   console.log('John wins on average');
+// } else if (averageMike > averageJohn && averageMike > averageMary) {
+//   console.log('Mike wins on average');
+// } else if (averageMary > averageJohn && averageMary > averageMike) {
+//   console.log('Mary wins on average');
+// } else {
+//   console.log('There is a draw');
+// }
+
+// ############################################
 // Lecture #
 // ********************************************
 
