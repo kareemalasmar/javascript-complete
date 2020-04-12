f37d884e7a0198c9bbf6efc5376bd316018aa4ff
KA <kareemalasmar@gmail.com>
2020-04-11 19:37:23 -0700

    hold btn

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -51,15 +51,45 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   } else {
     // Next Player
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    roundScore = 0;
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    // document.querySelector('.player-1-panel').classList.remove('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    document.querySelector('.dice').style.display = 'none';
+    nextPlayer();
   }
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+  // Update Player Score
+  scores[activePlayer] += roundScore;
+
+  // Display new Score
+  document.querySelector('#score-' + activePlayer).textContent =
+    scores[activePlayer];
+
+  // Check if player won the game
+  if (scores[activePlayer] >= 20) {
+    document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+    document.querySelector('.dice').style.display = 'none';
+    document
+      .querySelector('.player-' + activePlayer + '-panel')
+      .classList.add('winner');
+    document
+      .querySelector('.player-' + activePlayer + '-panel')
+      .classList.remove('active');
+  } else {
+    // Next Player
+    nextPlayer();
+  }
+});
+
+function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  //document.querySelector('.player-0-panel').classList.remove('active');
+  //document.querySelector('.player-1-panel').classList.add('active');
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
