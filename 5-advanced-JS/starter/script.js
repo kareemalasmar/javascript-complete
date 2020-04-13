13ffeab63f3be643abab92c541b949d118651407
KA <kareemalasmar@gmail.com>
2020-04-12 23:22:26 -0700

    object create method

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -54,6 +54,37 @@
 // console.log(mark.lastName);
 
 // ##########################################################
+// Lecture # 63         Object.create method
+// **********************************************************
+
+var personProto = {
+  // variable isnt capitalized because it is not a function constructor
+  calculateAge: function () {
+    console.log(2020 - this.yearOfBirth);
+  },
+};
+
+var john = Object.create(personProto);
+
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+console.log(john);
+
+var jane = Object.create(personProto, {
+  name: { value: 'Jane' },
+  yearOfBirth: { value: 1969 },
+  job: { value: 'designer' },
+});
+
+console.log(jane);
+
+// ##########################################################
+// Lecture #
+// **********************************************************
+
+// ##########################################################
 // Lecture #
 // **********************************************************
 
