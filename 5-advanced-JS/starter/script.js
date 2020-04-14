ca6ac27999eb88db16a5323451a4668cdd4ca845
KA <kareemalasmar@gmail.com>
2020-04-13 21:12:13 -0700

    first class functions

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -122,8 +122,42 @@
 // console.log(obj.city);
 
 // ##########################################################
-// Lecture #
+// Lecture # 65     First Class Functions
 // **********************************************************
+
+// // Functions as arguments
+// var years = [1990, 1965, 1973, 1989, 2006];
+
+// function arrayCalc(arr, fn) {
+//   var arrRes = [];
+//   for (var i = 0; i < arr.length; i++) {
+//     arrRes.push(fn(arr[i]));
+//   }
+//   return arrRes;
+// }
+
+// function calculateAge(el) {
+//   return 2020 - el;
+// }
+
+// function isFullAge(el) {
+//   return el >= 18;
+// }
+
+// function maxHeartRate(el) {
+//   if (el >= 18 && el <= 81) {
+//     return Math.round(206.9 - 0.67 * el);
+//   } else {
+//     return -1;
+//   }
+// }
+
+// var ages = arrayCalc(years, calculateAge);
+// var fullAges = arrayCalc(ages, isFullAge);
+// var heartRates = arrayCalc(ages, maxHeartRate);
+// console.log(ages);
+// console.log(fullAges);
+// console.log(heartRates);
 
 // ##########################################################
 // Lecture #
