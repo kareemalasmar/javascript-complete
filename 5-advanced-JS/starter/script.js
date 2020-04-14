a944cf1469e2892181b51834a7c5daaaba4b5aef
KA <kareemalasmar@gmail.com>
2020-04-13 23:09:25 -0700

    closures

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -213,8 +213,49 @@
 // })(5);
 
 // ##########################################################
-// Lecture #
+// Lecture # 68         Closures
 // **********************************************************
+
+// Keeps scope chain variables in memory even after function finished running
+
+// function retirement(retirementAge) {
+//   var a = ' years until retirement';
+//   return function (yearOfBirth) {
+//     var age = 2020 - yearOfBirth;
+//     console.log(retirementAge - age + a);
+//   };
+// }
+
+// var retirementUS = retirement(66);
+// // retirement(66)(1990);
+
+// var retirementGermany = retirement(65);
+// var retirementIceland = retirement(67);
+
+// retirementUS(1990);
+// retirementGermany(1990);
+// retirementIceland(1990);
+
+// function interviewQuestion(job) {
+//   return function (name) {
+//     if (job === 'designer') {
+//       console.log(name + ', can you please explain what UX design is?');
+//     } else if (job === 'teacher') {
+//       console.log('What subject do you teach, ' + name + '?');
+//     } else {
+//       console.log('Hello ' + name + ', what do you do?');
+//     }
+//   };
+// }
+
+// var teacherQuestion = interviewQuestion('teacher');
+
+// var designerQuestion = interviewQuestion('designer');
+
+// teacherQuestion('John');
+// designerQuestion('John');
+
+// interviewQuestion('teacher')('Mark');
 
 // ##########################################################
 // Extra Work
