9008c9357e7939d365e56dec015527080e9b1075
KA <kareemalasmar@gmail.com>
2020-04-15 16:12:52 -0700

    read input data

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -5,23 +5,46 @@ var budgetController = (function () {
 
 // UI CONTROLLER
 var UIController = (function () {
-  // some code
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn',
+  };
+
+  return {
+    // 1. Get input data
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    // Expose private DOMstrings to public through a method
+    getDomStrings: function () {
+      return DOMstrings;
+    },
+  };
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
+  // Get private variable from UIController
+  var DOM = UICtrl.getDomStrings();
   // Add item function
   var ctrlAddItem = function () {
     // 1. Get input Data
+    var input = UICtrl.getInput();
+    console.log(input);
     // 2. Add it to budget controller
     // 3. Display new item to UI
     // 4. Calculate new budget
     // 5. Display new budget
-    console.log('it worked');
   };
 
   // Event Listener for add button
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
   // Event handler to handle if someone presses enter
   document.addEventListener('keypress', function (event) {
