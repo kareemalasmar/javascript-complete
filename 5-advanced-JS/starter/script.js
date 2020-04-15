1bc93b0aa18234c34fe4baf66ad0568490dd956a
KA <kareemalasmar@gmail.com>
2020-04-14 23:18:09 -0700

    challenge 7 part 1

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -356,11 +356,84 @@
 // console.log(fullJapan);
 
 // ##########################################################
-// Lecture #
+// Lecture # Coding Challenge #7
 // **********************************************************
-// ##########################################################
-// Lecture #
-// **********************************************************
+
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+
+var Question = function (question, answers, correct) {
+  this.question = question;
+  this.answers = answers;
+  this.correct = correct;
+};
+
+Question.prototype.displayQuestion = function () {
+  console.log('Question: ' + this.question);
+  console.log('----------------------------------');
+  for (var i = 0; i < this.answers.length; i++) {
+    console.log(i + ') ' + this.answers[i]);
+    console.log('----------------------------------');
+  }
+};
+
+Question.prototype.checkAnswer = function (ans) {
+  if (ans === this.correct) {
+    alert('You win!');
+  } else {
+    alert('Wrong!');
+  }
+};
+
+var q1, q2, q3;
+
+q1 = new Question(
+  'What is Bojack?',
+  ['sad horse', 'angsty zebra', 'smoking camel'],
+  0
+);
+q2 = new Question(
+  'What is Cabracadabra?',
+  ['pasta brand', 'magician school', 'ride service app'],
+  2
+);
+q3 = new Question(
+  'What are you doing here?',
+  ['ugh', 'Errriiiiccaaaaa', 'Sarah Lynn?'],
+  1
+);
+
+var questions = [];
+
+questions.push(q1, q2, q3);
+
+var n = Math.floor(Math.random() * questions.length);
+
+questions[n].displayQuestion();
+
+var answer = parseInt(
+  prompt('Please enter the number besides the correct answer.')
+);
+
+questions[n].checkAnswer(answer);
 
 // ##########################################################
 // Extra Work
