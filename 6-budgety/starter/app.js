fb5043f27b852d350f81727e46b4b2899049ec30
KA <kareemalasmar@gmail.com>
2020-04-16 22:32:49 -0700

    addItem budgetController method

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -6,6 +6,7 @@ var budgetController = (function () {
     this.description = description;
     this.value = value;
   };
+
   var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
@@ -20,6 +21,35 @@ var budgetController = (function () {
     totals: {
       exp: 0,
       inc: 0,
+    },
+  };
+
+  return {
+    addItem: function (type, des, val) {
+      var newItem, ID;
+
+      // Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Create new item based on type
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+
+      // Push to corresponding array
+      data.allItems[type].push(newItem);
+
+      // Return newItem for acces in global controller
+      return newItem;
+    },
+    testing: function () {
+      console.log(data);
     },
   };
 })();
@@ -69,10 +99,17 @@ var controller = (function (budgetCtrl, UICtrl) {
 
   // Add item function
   var ctrlAddItem = function () {
+    var input, newItem;
     // 1. Get input Data
-    var input = UICtrl.getInput();
-    console.log(input);
+    input = UICtrl.getInput();
+    // var { type, description, value } = input;
+    // console.log(type, description, value);
     // 2. Add it to budget controller
+    newItem = budgetController.addItem(
+      input.type,
+      input.description,
+      input.value
+    );
     // 3. Display new item to UI
     // 4. Calculate new budget
     // 5. Display new budget
