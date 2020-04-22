7ed9a2c09ba485dc0c3e933effc8a2a6b9ca8c5f
KA <kareemalasmar@gmail.com>
2020-04-22 14:58:15 -0700

    destructuring

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -244,6 +244,46 @@
 // // arrow functions do not have their own this and take parents
 
 // ##########################################################
+// Lecture # 109  Destructuring
+// **********************************************************
+
+// // ES5
+// var john = ['James', 27];
+// // var name = john[0];
+// // var age = john[0];
+
+// // ES6
+// // left side destructures the data object to the right
+// const [name, age] = ['John', 26];
+// console.log(name);
+// console.log(age);
+
+// const obj = {
+//   firstName: 'Mark',
+//   lastName: 'Frost',
+// };
+
+// // these variable names have t0 match obj keys
+// const { firstName, lastName } = obj;
+// console.log(firstName);
+// console.log(lastName);
+
+// // If you want different variable names
+// const { firstName: a, lastName: b } = obj;
+// console.log(a);
+// console.log(b);
+
+// // You can destructure calues returned from a function
+// function calcAgeRetirement(year) {
+//   const age = new Date().getFullYear() - year;
+//   return [age, 65 - age];
+// }
+
+// const [age2, retirement] = calcAgeRetirement(1990);
+// console.log(age2);
+// console.log(retirement);
+
+// ##########################################################
 // Lecture #
 // **********************************************************
 
