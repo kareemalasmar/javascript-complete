cd835f968eb7be651afed06e1f7dbe8e542f7414
KA <kareemalasmar@gmail.com>
2020-04-21 21:35:47 -0700

    arrow functions

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -123,6 +123,42 @@
 // console.log(`${firstName} `.repeat(5));
 
 // ##########################################################
+// Lecture # 107 Arrow function
+// **********************************************************
+
+// const years = [1990, 1965, 1982, 1937];
+
+// // ES5
+// var ages5 = years.map(function (cur) {
+//   return 2020 - cur;
+// });
+
+// console.log(ages5);
+
+// // ES6
+// const ages6 = years.map((el) => 2020 - el);
+// console.log(ages6);
+
+// // With more than 1 argument you must put them in ()
+// const ages6b = years.map(
+//   (el, index) => `Age element ${index + 1}: ${2020 - el}.`
+// );
+// console.log(ages6b);
+
+// // With more than one line in return you need to include 'return' and curly braces
+// const ages6c = years.map((el, index) => {
+//   const now = new Date().getFullYear();
+//   const age = now - el;
+//   return `Age element ${index + 1}: ${age}.`;
+// });
+
+// console.log(ages6c);
+
+// ##########################################################
+// Lecture #
+// **********************************************************
+
+// ##########################################################
 // Lecture #
 // **********************************************************
 
