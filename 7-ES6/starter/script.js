2fdbad5d74905303d5673b197e3f7b9aaa7da7d6
KA <kareemalasmar@gmail.com>
2020-04-23 14:56:46 -0700

    es6 subclasses and extends

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -552,8 +552,70 @@
 // const john6 = new Person5('John', 1990, 'developer');
 
 // ##########################################################
-// Lecture #
+// Lecture # 116  Classes with SubClasses
 // **********************************************************
+// // ES5
+// var Person5 = function (name, yearOfBirth, job) {
+//   this.name = name;
+//   this.yearOfBirth = yearOfBirth;
+//   this.job = job;
+// };
+
+// Person5.prototype.calculateAge = function () {
+//   var age = new Date().getFullYear() - this.yearOfBirth;
+//   console.log(age);
+// };
+
+// var Athlete5 = function (name, yearOfBirth, job, games, medals) {
+//   Person5.call(this, name, yearOfBirth, job);
+//   this.games = games;
+//   this.medals = medals;
+// };
+
+// // Connects prototype chain
+// Athlete5.prototype = Object.create(Person5.prototype);
+
+// Athlete5.prototype.wonMedal = function () {
+//   this.medals++;
+//   console.log(this.medals);
+// };
+
+// var johnAthlete5 = new Athlete5('John', 1990, 'swimmer', 3, 10);
+
+// // Athlte still has access to Person proto
+// johnAthlete5.calculateAge();
+// johnAthlete5.wonMedal();
+
+// // ES6
+
+// class Person6 {
+//   constructor(name, yearOfBirth, job) {
+//     this.name = name;
+//     this.yearOfBirth = yearOfBirth;
+//     this.job = job;
+//   }
+//   calculateAge() {
+//     var age = new Date().getFullYear() - this.yearOfBirth;
+//     console.log(age);
+//   }
+// }
+
+// class Athlete6 extends Person6 {
+//   constructor(name, yearOfBirth, job, games, medals) {
+//     super(name, yearOfBirth, job); // takes it out of Person6 and sets 'this'
+//     this.games = games;
+//     this.medals = medals;
+//   }
+//   wonMedals() {
+//     this.medals++;
+//     console.log(this.medals);
+//   }
+// }
+
+// const johnAthlete6 = new Athlete6('John', 1990, 'swimmer', 3, 10);
+
+// johnAthlete6.calculateAge();
+// johnAthlete6.wonMedals();
 
 // ##########################################################
 // Lecture #
