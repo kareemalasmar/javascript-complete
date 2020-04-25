4b83060d4b9a7f581a7cdbfba609c4b033143e9b
KA <kareemalasmar@gmail.com>
2020-04-24 23:35:36 -0700

    promises then and catch

--- 8-asynchronous-JS/starter/script.js
+++ 8-asynchronous-JS/starter/script.js
@@ -31,7 +31,7 @@
 //           title: 'Fresh tomato pasta',
 //           publisher: 'Jonas',
 //         };
-//         console.log(`${id}: ${recipe.title}. Published by ${recipe.publisher}`);
+//         console.log(`${id}: ${recipe.title}`);
 //         setTimeout(
 //           (publisher) => {
 //             const recipe2 = { title: 'Italian pizza', publisher: 'Jonas' };
@@ -53,10 +53,73 @@
 // ES6 provides promises to help with this
 
 // ##########################################################
+// Lecture # 123  Promises, then and catch
+// **********************************************************
+
+// // A promise is an object that keeps track about whether a certain event has happened already or not. If it did happen the promise determines what happens next
+// // Before the event the promise is pending. After the event the promise is settled or resolved. If the result is available the promise is fulfilled if not it is rejected.
+
+// // Executor is a functin that is immediatley called when the promise is created. executor takes two arguments for if promise was succesful or not
+// const getIDs = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve([523, 883, 432, 974]);
+//   }, 1500);
+// });
+
+// const getRecipe = (recID) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(
+//       (ID) => {
+//         const recipe = {
+//           title: 'Fresh tomato pasta',
+//           publisher: 'Jonas',
+//         };
+//         resolve(`${ID}: ${recipe.title}`);
+//       },
+//       1500,
+//       recID
+//     );
+//   });
+// };
+// // since setTimeout never fails there is no need for the reject here. the resolve will return the arr
+// // Then and catch methods are attached to all promise objects
+// // Then allows us to have an event handler for the case the promise is fulfilled
+
+// const getRelated = (publisher) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(
+//       (pub) => {
+//         const recipe = { title: 'Italian pizza', publisher: 'Jonas' };
+//         resolve(`${pub}: ${recipe.title}`);
+//       },
+//       1500,
+//       publisher
+//     );
+//   });
+// };
+
+// getIDs
+//   .then((IDs) => {
+//     // IDs will be the result of a succesful promise aka the arr
+//     console.log(IDs);
+//     return getRecipe(IDs[2]);
+//   })
+//   .then((recipe) => {
+//     console.log(recipe);
+//     return getRelated('Jonas');
+//   })
+//   .then((recipe) => {
+//     console.log(recipe);
+//   })
+//   // Catch allows us to have an event handler for the case the promise is rejected
+//   .catch((error) => {
+//     console.log('Error');
+//   });
+
+// ##########################################################
 // Lecture #
 // **********************************************************
 
-// ##########################################################
 // ##########################################################
 // Lecture #
 // **********************************************************
